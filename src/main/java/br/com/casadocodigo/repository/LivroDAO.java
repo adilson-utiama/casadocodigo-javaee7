9bde8923e9f02a63047ec1fdb091937c2abc29d3
Adilson S. Utiama <adilson.utiama@gmail.com>
2017-10-16 23:56:14 -0200

    Limpando o cache

--- src/main/java/br/com/casadocodigo/repository/LivroDAO.java
+++ src/main/java/br/com/casadocodigo/repository/LivroDAO.java
@@ -3,12 +3,14 @@ package br.com.casadocodigo.repository;
 import java.util.List;
 
 import javax.ejb.Stateful;
+import javax.persistence.Cache;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 import javax.persistence.QueryHint;
 import javax.transaction.Transactional;
 
+import org.hibernate.SessionFactory;
 import org.hibernate.jpa.QueryHints;
 
 import br.com.casadocodigo.model.Livro;
@@ -32,12 +34,25 @@ public class LivroDAO {
 
         return manager.createQuery(jpql, Livro.class).getResultList();
 	}
+	
+	private void LimpaCacheJPA() {
+		Cache cache = manager.getEntityManagerFactory().getCache();
+		//cache.evict(Livro.class); Limpa uma determinada entidade do cache
+		cache.evictAll(); //limpa todo o cache
+	}
+	
+	private void limpaCAcheHibernate() {
+		SessionFactory factory = manager.getEntityManagerFactory().unwrap(SessionFactory.class);
+		factory.getCache().evictAllRegions(); //Limpa todo o cache
+		//factory.getCache().evictQueryRegion("home"); //Limpa uma determinada regiao do cache, no caso todas associadas a home
+	}
 
 	public List<Livro> ultimosLancamentos() {
 		String jpql = "select l from Livro l order by l.id desc";
 	    return manager.createQuery(jpql, Livro.class)
 	            .setMaxResults(5)
 	            .setHint(QueryHints.HINT_CACHEABLE,	true)
+	            //.setHint(QueryHints.HINT_CACHE_REGION, "home") Setando a Regiao a limpar cache
 	            .getResultList();
 	}
 
@@ -46,6 +61,7 @@ public class LivroDAO {
         return manager.createQuery(jpql, Livro.class)
         		.setFirstResult(5)
         		.setHint(QueryHints.HINT_CACHEABLE,	true)
+        		//.setHint(QueryHints.HINT_CACHE_REGION, "home") Setando a Regiao a limpar cache
                 .getResultList();
 	}
 
