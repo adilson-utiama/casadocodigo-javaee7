d4b658205bb2328a27de4a352ebba73faf948cfb
Adilson S. Utiama <adilson.utiama@gmail.com>
2017-10-20 00:56:19 -0200

    Protegendo area administrativa com JASS -preparacao

---
+++ src/main/java/br/com/casadocodigo/model/SystemUser.java
@@ -0,0 +1,54 @@
+package br.com.casadocodigo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.validator.constraints.Email;
+
+@Entity
+public class SystemUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Email
+	private String login;
+
+	private String senha;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<SystemRole> roles = new ArrayList<>();
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<SystemRole> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<SystemRole> roles) {
+		this.roles = roles;
+	}
+
+}
